54ae47805b8e38ac04e7fa49f11ca3c1c4719eb4
07prashantg <prashantgoswami794@gmail.com>
2023-07-19 13:22:23 +0530

    second commit

---
+++ Component/EmployeeLogin.js
@@ -0,0 +1,20 @@
+const EmployeeLogin = (props) => {
+    return (
+        <>
+            <div className="EmployeeLogin">
+                <div>
+                    <img className="Employeeimage" src="https://wallpapers-clan.com/wp-content/uploads/2023/02/anime-boy-black-pfp-1.jpg"></img>
+                </div>
+                <div className="employeeDetail">
+                    <div className="flex-item">Name : Prashant</div>
+                    <div className="flex-item">Email : xyz@gmail.com</div>
+                    <div className="flex-item">Employee Id : 1234</div>
+                    <div className="flex-item">Department : IT</div>
+                    <div className="flex-item">Date of joining : 12/23/1212</div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+export default EmployeeLogin;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+const EmployeeList = [
+    {
+        Name : "Car",
+        Email : "xyz@gmail.com",
+        EmployeeId : "123456789",
+        Department : "xyz",
+        "Date of joining" : "12/13/1334"
+    },
+    {
+        Name : "Bike",
+        Email : "xyz@gmail.com",
+        EmployeeId : "1234567",
+        Department : "xyzsdc",
+        "Date of joining" : "12/13/1354"
+    },
+    {
+        Name : "Auto",
+        Email : "xyz@gmail.com",
+        EmployeeId : "1234",
+        Department : "xyzcaqdw",
+        "Date of joining" : "12/13/1324"
+    },
+    {
+        Name : "Metro",
+        Email : "xyz@gmail.com",
+        EmployeeId : "98765",
+        Department : "nndnjns",
+        "Date of joining" : "98/65/9876"
+    }
+
+]
+export default EmployeeList;
